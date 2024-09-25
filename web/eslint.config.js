8ede37983d5327508643af7741f0e6e6e8a9a4ed
NikX <41104633+NikXpro@users.noreply.github.com>
2024-09-25 20:08:27 +0200

    chore: update eslint.config.js

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,28 +1,48 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
-  },
-)
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".ts", ".jsx", ".tsx"],
+          moduleDirectory: ["node_modules", "src"], // Ajoutez le répertoire src pour les alias
+          map: [
+            ["@hooks", "./src/hooks"],
+            ["@utils", "./src/utils"],
+            ["@assets", "./src/assets"],
+            ["@providers", "./src/providers"],
+            ["@components", "./src/components"],
+            ["@shared", "./src/components/shared"],
+            ["@ui", "./src/components/ui"],
+            ["@pages", "./src/pages"],
+            ["@public", "./public/"],
+            ["@", "./src"],
+          ],
+        },
+      },
+    },
+  }
+);
